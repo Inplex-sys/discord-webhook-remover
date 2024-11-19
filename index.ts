a0fbdfcb70367036dd1df61c97b3f0b688fd582a
Inplex-sys <anthill-spud-puma@duck.com>
2024-11-19 22:16:00 +0100

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,13 +1,15 @@
-const request = require('request');
-const path = require('path');
-const chalk = require('chalk');
+const request = require("request");
+const path = require("path");
+const chalk = require("chalk");
 
-console.log(chalk.red(`
+console.log(
+    chalk.red(`
   ╔╦╗╔═╗╦═╗╦╔═    ╦ ╦╔╦╗╦╦  ╦╔╦╗╦╔═╗╔═╗
    ║║╠═╣╠╦╝╠╩╗    ║ ║ ║ ║║  ║ ║ ║║╣ ╚═╗
   ═╩╝╩ ╩╩╚═╩ ╩    ╚═╝ ╩ ╩╩═╝╩ ╩ ╩╚═╝╚═╝
        The power on your side
-`))
+`),
+);
 
 class Main {
     static formatConsoleDate(date) {
@@ -16,15 +18,17 @@ class Main {
         var seconds = date.getSeconds();
         var milliseconds = date.getMilliseconds();
 
-        return '[' +
-            ((hour < 10) ? '0' + hour : hour) +
-            ':' +
-            ((minutes < 10) ? '0' + minutes : minutes) +
-            ':' +
-            ((seconds < 10) ? '0' + seconds : seconds) +
-            '.' +
-            ('00' + milliseconds).slice(-3) +
-            '] ';
+        return (
+            "[" +
+            (hour < 10 ? "0" + hour : hour) +
+            ":" +
+            (minutes < 10 ? "0" + minutes : minutes) +
+            ":" +
+            (seconds < 10 ? "0" + seconds : seconds) +
+            "." +
+            ("00" + milliseconds).slice(-3) +
+            "] "
+        );
     }
 
     static GetArgs() {
@@ -33,36 +37,45 @@ class Main {
 }
 
 class Discord {
-	static DeleteWebhook( url ) {
-		return new Promise(function(resolve, reject) {
-			request.delete( url, function optionalCallback(err, httpResponse, body) {
-				try {
-					if (body.includes('Unknown Webhook')){
-						resolve(false)
-					}else{
-						resolve(true)
-					}
-				}catch {
-					resolve(false)
-				}
-			})
-		});
-	}
+    static DeleteWebhook(url) {
+        return new Promise(function (resolve, reject) {
+            request.delete(
+                url,
+                function optionalCallback(err, httpResponse, body) {
+                    try {
+                        if (body.includes("Unknown Webhook")) {
+                            resolve(false);
+                        } else {
+                            resolve(true);
+                        }
+                    } catch {
+                        resolve(false);
+                    }
+                },
+            );
+        });
+    }
 }
 
 if (Main.GetArgs().length < 1) {
     console.log(`${chalk.red("[ERROR]")} bad command usage
 
-        ${chalk.yellow('Usage Sheme:')}
+        ${chalk.yellow("Usage Sheme:")}
             - user@some_name:~# node ${path.basename(__filename)} <webhook-url>
     `);
     process.exit(0);
 }
 
-Discord.DeleteWebhook( Main.GetArgs()[0] ).then( response => {
-	if (response) {
-		console.log(chalk.hex('#d6af42')(Main.formatConsoleDate(new Date())) + chalk.green("Webhook successfully deleted !"))
-	}else{
-		console.log(chalk.hex('#d6af42')(Main.formatConsoleDate(new Date())) + chalk.red("Webhook does not exist ..."))
-	}
-})
+Discord.DeleteWebhook(Main.GetArgs()[0]).then((response) => {
+    if (response) {
+        console.log(
+            chalk.hex("#d6af42")(Main.formatConsoleDate(new Date())) +
+                chalk.green("Webhook successfully deleted !"),
+        );
+    } else {
+        console.log(
+            chalk.hex("#d6af42")(Main.formatConsoleDate(new Date())) +
+                chalk.red("Webhook does not exist ..."),
+        );
+    }
+});
